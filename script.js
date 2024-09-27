06ff3224f69bf8f0a96c6e2ac3b8f2f279aa7f01
The-bAk-Dev <hamadi.the.music.therapist@gmail.com>
2024-09-27 12:25:03 +0200

    sections are being implemented so far

---
+++ script.js
@@ -0,0 +1,25 @@
+let serviceCard1 = document.querySelector('.service_1');
+let serviceCard2 = document.querySelector('.service_2');
+let serviceCard3 = document.querySelector('.service_3');
+
+let serviceLink1 = document.querySelector('.service_link1');
+let serviceLink2 = document.querySelector('.service_link2');
+let serviceLink3 = document.querySelector('.service_link3');
+
+serviceLink1.addEventListener('click', () => {
+  serviceCard1.style.display = 'flex';
+  serviceCard2.style.display = 'none';
+  serviceCard3.style.display = 'none';
+});
+
+serviceLink2.addEventListener('click', () => {
+  serviceCard1.style.display = 'none';
+  serviceCard2.style.display = 'flex';
+  serviceCard3.style.display = 'none';
+});
+
+serviceLink3.addEventListener('click', () => {
+  serviceCard1.style.display = 'none';
+  serviceCard2.style.display = 'none';
+  serviceCard3.style.display = 'flex';
+});
